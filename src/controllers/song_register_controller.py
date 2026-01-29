c83029263f9c9e78244e74ea1d9bc40fad6e5902
Blenda Cunha <blenda.cunha13@gmail.com>
2026-01-29 12:34:30 -0300

    feat: Relating song_register_view and song_register_controller in main

--- src/controllers/song_register_controller.py
+++ src/controllers/song_register_controller.py
@@ -2,14 +2,14 @@ class SongRegisterController:
     def insert(self, new_song_informations: dict) -> dict:
         # Principio da responsabilidade única
         try:
-            self.__veriffy_songs_infos(new_song_informations)
+            self.__verify_songs_infos(new_song_informations)
             self.__verify_if_song_already_registered(new_song_informations)
             self.__insert_song(new_song_informations)
             return self.__format_response(new_song_informations)
         except Exception as exception:
             return self.__format_error_response(exception)
 
-    def __veriffy_songs_infos(self, new_song_informations: dict) -> None:
+    def __verify_songs_infos(self, new_song_informations: dict) -> None:
         if len(new_song_informations["title"]) > 100:
             raise Exception("Título de música com mais de 100 caracteres")
         
@@ -27,15 +27,15 @@ class SongRegisterController:
 
     def __format_response(self, new_song_informations: dict) -> dict:
         return {
-            "sucess": True,
+            "success": True,
             "count": 1,
             "attributes": {
                 "title": new_song_informations["title"]
             }
         }
     
-    def __format_error_response(self, err:Exception) -> dict:
+    def __format_error_response(self, err: Exception) -> dict:
         return{
-            "sucess": False,
-            "error": str(exception)
+            "success": False,
+            "error": str(err)
         }
--- src/main/constructor/song_register_constructor.py
+++ src/main/constructor/song_register_constructor.py
@@ -1,8 +1,14 @@
 from src.view.song_register_view import SongRegisterView
+from src.controllers.song_register_controller import SongRegisterController
 
 def song_register_process():
     song_register_view = SongRegisterView()
-    #Instancia do controller
+    song_register_controller = SongRegisterController()
 
     new_song_informations = song_register_view.registry_song_initial()
-    # enviar new_song_informations para controller
+    response = song_register_controller.insert(new_song_informations)
+
+    if response["success"]:
+        song_register_view.registry_song_success(response)
+    else: 
+        song_register_view.registry_song_fail(response)
